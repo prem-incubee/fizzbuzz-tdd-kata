94aa9e5bcd5d7c0a22754452f65787a57475233e
premg <prem@incubyte.co>
2024-12-21 13:18:59 +0530

    test case with input 2 and 4

--- src/__tests__/fizzbuzz.test.ts
+++ src/__tests__/fizzbuzz.test.ts
@@ -4,4 +4,12 @@ describe('fizzBuzz test suite', () => {
     it('should return "1" for input 1', () => {
         expect(fizzBuzz(1)).toBe("1");
     });
+
+    it('should return "2" for input 2', () => {
+        expect(fizzBuzz(2)).toBe("2");
+    });
+
+    it('should return "4" for input 4', () => {
+        expect(fizzBuzz(4)).toBe("4");
+    });
 });
