df58af9b167b67c7d2491eff27fbf7d2ffed3ef7
premg <prem@incubyte.co>
2024-12-21 13:36:51 +0530

    passing test cases for multiples of 5

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,5 +1,5 @@
 export default function fizzBuzz(num: number) {
   if (num % 3 === 0) return "Fizz";
-  if (num === 5) return "Buzz";
+  if (num % 5 === 0) return "Buzz";
   return num.toString();
 }
