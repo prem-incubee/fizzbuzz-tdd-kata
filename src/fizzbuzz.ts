ac88704d81b306c0fbc7afae89ca44367d57d0e2
premg <prem@incubyte.co>
2024-12-21 13:49:03 +0530

    refactored fizzbuzz condition check

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,5 +1,5 @@
 export default function fizzBuzz(num: number) {
-    if (num % 3 === 0 && num % 5 === 0) {
+    if (num % 15 === 0) {
       return "FizzBuzz";
     } else if (num % 3 === 0) {
       return "Fizz";
