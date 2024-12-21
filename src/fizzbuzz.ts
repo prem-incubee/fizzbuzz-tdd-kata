137e1c8c68d0a07baf6c6e1059b768c62b463bd3
premg <prem@incubyte.co>
2024-12-21 13:46:30 +0530

    refactor fizzBuzz function

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,6 +1,11 @@
 export default function fizzBuzz(num: number) {
-  if (num % 3 === 0 && num % 5 === 0) return "FizzBuzz";
-  if (num % 3 === 0) return "Fizz";
-  if (num % 5 === 0) return "Buzz";
-  return num.toString();
-}
+    if (num % 3 === 0 && num % 5 === 0) {
+      return "FizzBuzz";
+    } else if (num % 3 === 0) {
+      return "Fizz";
+    } else if (num % 5 === 0) {
+      return "Buzz";
+    } else {
+      return num.toString();
+    }
+  }
