acd83c9199bdc5612113400dbf8cf96073aead4c
premg <prem@incubyte.co>
2024-12-26 13:39:18 +0530

    test case for prime factor of 6

--- src/__tests__/primeFactors.test.ts
+++ src/__tests__/primeFactors.test.ts
@@ -20,7 +20,11 @@ describe("prime factors for a given natural number", () => {
   it("prime factor of 5 should be list of 5", () => {
     expect(factorsOf(5)).toEqual([5])
   });
-  it.todo("prime factor of 6 should be list of 2,3");
+
+  it("prime factor of 6 should be list of 2,3", () => {
+    expect(factorsOf(6)).toEqual([2,3])
+  });
+
   it.todo("prime factor of 7 should be list of 7");
   it.todo("prime factor of 8 should be list of 2,2,2");
   it.todo("prime factor of 9 should be list of 3,3");
