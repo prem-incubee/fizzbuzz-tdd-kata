8f8a07e1358108a29e4b4c04fdcf585f6c53900b
premg <prem@incubyte.co>
2024-12-26 13:39:51 +0530

    test case for prime factor of 7

--- src/__tests__/primeFactors.test.ts
+++ src/__tests__/primeFactors.test.ts
@@ -25,7 +25,10 @@ describe("prime factors for a given natural number", () => {
     expect(factorsOf(6)).toEqual([2,3])
   });
 
-  it.todo("prime factor of 7 should be list of 7");
+  it("prime factor of 7 should be list of 7", () => {
+    expect(factorsOf(7)).toEqual([7])
+  });
+
   it.todo("prime factor of 8 should be list of 2,2,2");
   it.todo("prime factor of 9 should be list of 3,3");
 });
