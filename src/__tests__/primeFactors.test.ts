bdc459af9347ed271b6b96012d9f04edde8f4c6f
premg <prem@incubyte.co>
2024-12-26 12:37:35 +0530

    test case for prime factor of 2

--- src/__tests__/primeFactors.test.ts
+++ src/__tests__/primeFactors.test.ts
@@ -5,7 +5,10 @@ describe("prime factors for a given natural number",() => {
         expect(factorsOf(1)).toEqual([])
     })
 
-    it.todo("prime factor of 2 should be 2")
+    it("prime factor of 2 should be list of 2", () => {
+        expect(factorsOf(2)).toEqual([2])
+    })
+
     it.todo("prime factor of 3 should be 3")
     it.todo("prime factor of 4 should be 2,2")
     it.todo("prime factor of 5 should be 5")
--- src/katas/primeFactors.ts
+++ src/katas/primeFactors.ts
@@ -1,3 +1,4 @@
 export default function factorsOf(number: number) {
   if (number === 1) return [];
+  if (number === 2) return [2];
 }
