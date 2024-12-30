be51e7b540accd2c90a9ffe7e93cd939b068f5ca
premg <prem@incubyte.co>
2024-12-30 10:58:14 +0530

    test case for single digit numeric string 7

--- src/__tests__/stringCalculator.test.ts
+++ src/__tests__/stringCalculator.test.ts
@@ -4,4 +4,8 @@ describe("string calculator test suite", () => {
   it("returns 0 for empty string", () => {
     expect(stringCalculator("")).toBe(0);
   });
+
+  it("returns 7 for input string 7", () => {
+    expect(stringCalculator("7")).toBe(7);
+  });
 });
--- src/katas/stringCalculator.ts
+++ src/katas/stringCalculator.ts
@@ -1,3 +1,5 @@
 export default function stringCalculator(listOfNumbers: string) {
   if (listOfNumbers === "") return 0;
+
+  return Number(listOfNumbers);
 }
