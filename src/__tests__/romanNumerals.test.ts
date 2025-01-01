fc8e4f998a749fb04d379224dfde17e520bab9c4
premg <prem@incubyte.co>
2025-01-01 21:45:45 +0530

    test case for should return IX for 9

--- src/__tests__/romanNumerals.test.ts
+++ src/__tests__/romanNumerals.test.ts
@@ -4,4 +4,8 @@ describe("get roman numerals for a given number", () => {
   it("should return IV for 4", () => {
     expect(convertToRomanNumerals(4)).toBe("IV");
   });
+
+  it("should return IX for 9", () => {
+    expect(convertToRomanNumerals(9)).toBe("IX");
+  });
 });
